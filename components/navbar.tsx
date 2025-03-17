3f6a2a24599c4149fd3a39c2198a8e0fded55667
Alfarabx <alfarabi1074@gmail.com>
2025-03-17 16:05:53 +0700

    Bug Fixes Deployment Failure

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,5 @@
 "use client";
-import React, { useEffect, useState } from 'react';
-import Cookies from 'js-cookie';
+import React, { useState } from 'react';
 import DisplayAddon from './addons/display';
 
 interface NavbarComponentProps {
